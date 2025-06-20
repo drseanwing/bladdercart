b65039cd2e5c05fd0021ff93d1aa2240a19c8c78
drseanwing <drseanwing@gmail.com>
2025-06-20 14:19:05 +1000

    Update uts.mjs

--- uts.mjs
+++ uts.mjs
@@ -5,10 +5,35 @@ import * as documents from "./src/module/documents/_module.mjs";
 import * as dataModels from "./src/module/data/_module.mjs";
 import {localizeHelper} from "./src/module/helpers/utils.mjs";
 
+import { DaggerheartActor } from "./actor/daggerheart-actor.js";
+import { DaggerheartItem } from "./item/daggerheart-item.js";
+
+import { DaggerheartCharacterSheet } from "./actor/character-sheet.js";
+import { DaggerheartAdversarySheet } from "./actor/adversary-sheet.js";
+
+import { registerDiceHelpers } from "./helpers/dice.js";
+import { registerRestHandlers } from "./helpers/rest.js";
+import { registerEvasion } from "./helpers/evasion.js";
+
 Hooks.once("init", () => {
   CONFIG.UTS = UTS;
   game.system.socketHandler = new UTSSocketHandler();
 
+  console.log("Daggerheart | Initializing system...");
+
+  CONFIG.Actor.documentClass = DaggerheartActor;
+  CONFIG.Item.documentClass = DaggerheartItem;
+
+  Actors.unregisterSheet("core", ActorSheet);
+  Items.unregisterSheet("core", ItemSheet);
+
+  Actors.registerSheet("daggerheart", DaggerheartCharacterSheet, { types: ["character"], makeDefault: true });
+  Actors.registerSheet("daggerheart", DaggerheartAdversarySheet, { types: ["adversary"], makeDefault: true });
+
+  registerDiceHelpers();
+  registerRestHandlers();
+  registerEvasion();
+  
   // Assign document classes
   for (const docCls of Object.values(documents)) {
     CONFIG[docCls.documentName].documentClass = docCls;
